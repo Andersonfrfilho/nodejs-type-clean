61f0a38bcd81dbfcf64b93c3fe0ef789ce93cb75
Anderson Fernandes <andersonfrfilho@gmail.com>
2022-11-07 22:14:26 -0300

    refactor: change import

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,15 +1,17 @@
-import { Controller, HttpRequest, HttpResponse, EmailValidator } from '../../protocol'
-import { MissingParamError, InvalidParamError } from '../errors'
-import { badRequest, serverError } from '../helpers/http-helper'
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
+import { AddAccount, Controller, EmailValidator, HttpRequest, HttpResponse } from './signup-protocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
-  handle (httpRequest: HttpRequest): HttpResponse {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -19,7 +21,7 @@ export class SignUpController implements Controller {
         }
       }
 
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
 
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
@@ -30,6 +32,12 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      const account = await this.addAccount.add({
+        name, email, password
+      })
+
+      return ok(account)
     } catch (error) {
       return serverError()
     }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,16 +1,17 @@
-import { HttpResponse } from '../../protocol/http'
-import { ServerError } from '../errors/server-error'
+import { ServerError } from '../errors'
+import { HttpResponse } from '../protocols'
 
-export const badRequest = (error: Error): HttpResponse => {
-  return {
-    statusCode: 400,
-    body: error
-  }
-}
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
 
-export const serverError = (): HttpResponse => {
-  return {
-    statusCode: 500,
-    body: new ServerError()
-  }
-}
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
